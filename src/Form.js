200019343a1793613ec23f61b46de6cd64a6a6bd
ashutoshmer <ashutosh@krishaweb.net>
2022-04-27 16:05:17 +0530

    React Switch Pushed

---
+++ src/Form.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <>
+      <div className="wrapper">
+        <div class="main">
+          <p class="sign" align="center">
+            Sign in
+          </p>
+          <form class="form1">
+            <input
+              class="username "
+              type="text"
+              align="center"
+              placeholder="Username"
+            />
+            <input
+              class="password"
+              type="password"
+              align="center"
+              placeholder="Password"
+            />
+            <a class="submit" align="center">
+              Sign in
+            </a>
+            <div class="forgot" align="center">
+              <a href="#">Forgot Password?</a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Form;
